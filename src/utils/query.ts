22443691d5a592fbff74e524617e411a20a8ed42
Greensky <david.hesliere@gmail.com>
2023-04-27 09:49:45 +0200

    chore: :card_file_box: Make database system

---
+++ src/utils/query.ts
@@ -0,0 +1,26 @@
+import { config } from 'dotenv'
+import { createConnection } from 'mysql'
+import { DefaultQueryResult, QueryResult } from '../typings/database';
+config()
+
+const database = createConnection({
+    user: process.env.dbu,
+    host: process.env.dbh,
+    password: process.env.dbp,
+    database: process.env.db
+});
+
+database.connect((error) => {
+    if (error) {
+        throw error
+    }
+})
+
+export const query = <T extends any = DefaultQueryResult>(query: string): Promise<QueryResult<T>> => {
+    return new Promise((resolve, reject) => {
+        database.query(query, (error, result) => {
+            if (error) return reject(error);
+            resolve(result);
+        })
+    })
+}
